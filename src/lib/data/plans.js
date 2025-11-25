9f3f91949a5236ea2b71d2e1ff488f4b806b0ecc
bruno <bruno.santos382@fatec.sp.gov.br>
2025-11-25 19:33:36 -0300

    lower price

--- src/lib/data/plans.js
+++ src/lib/data/plans.js
@@ -22,7 +22,7 @@ export const plans = {
     name: "Corporativo",
     subtitle: "(Avançado)",
     icon: "🚚",
-    price: 0.1,
+    price: 1.50,
     priceUnit: "/mês",
     description:
       "Para grandes frotas com foco em análise e performance avançada.",
